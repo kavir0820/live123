b1c8af84d1740fb764f660496df4291067d70c08
System Administrator <root@kavir-mbp.local>
2015-12-15 22:32:21 +0800

    修改游戏专区titinfo

--- resources/views/_layouts/default.blade.php
+++ resources/views/_layouts/default.blade.php
@@ -45,7 +45,7 @@
 <script>
     $(document).ready(function() {
         var pathname = window.location.pathname;
-        // var param_name = getUrlParam('name');
+        var param_name = getUrlParam('name');
         // $("#nav-tab li").each(function () {
         //     if ($(this).hasClass("active")) {
         //         $(this).removeClass("active");
@@ -57,21 +57,21 @@
         //     }
         // });
 
-        // if (pathname == '/games') {
-        //     // 设置游戏专区下的游戏筛选标签
-        //     $(".topbar-tabs .topbar-tab").each(function () {
-        //         if ($(this).hasClass("topbar-tab-selected") && param_name != null) {
-        //             $(this).removeClass("topbar-tab-selected");
-        //         }
-        //         var cur_href = $(this).children("a").attr("href");
-        //         if (cur_href.indexOf(param_name) >= 0) {
-        //             $(this).addClass("topbar-tab-selected");
-        //             return false;
-        //         }
-        //     });
-        //     // 设置living-tit下的h3内容
-        //     $(".living-tit h3").html((param_name != null && param_name != "全部") ? param_name : "全部直播");
-        // }
+         if (pathname == '/games') {
+             // 设置游戏专区下的游戏筛选标签
+//             $(".topbar-tabs .topbar-tab").each(function () {
+//                 if ($(this).hasClass("topbar-tab-selected") && param_name != null) {
+//                     $(this).removeClass("topbar-tab-selected");
+//                 }
+//                 var cur_href = $(this).children("a").attr("href");
+//                 if (cur_href.indexOf(param_name) >= 0) {
+//                     $(this).addClass("topbar-tab-selected");
+//                     return false;
+//                 }
+//             });
+             // 设置living-tit下的h3内容
+             $(".living-tit h3").html((param_name != null && param_name != "全部") ? param_name : "全部直播");
+         }
 
         // var query_val = getUrlParam('query');
         // $("#navform input").val(query_val);
